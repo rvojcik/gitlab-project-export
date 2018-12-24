d4c6f7a09447245be1eedcae829913851e234ee4
wiro <wiro@livesport.eu>
2018-12-24 14:06:48 +0100

    project import script

---
+++ gitlab-project-import.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+import os
+import argparse
+import yaml
+from datetime import date
+import requests
+# Find our libs
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from lib import config, gitlab
+
+
+return_code = 0
+
+if __name__ == '__main__':
+    # Parsing arguments
+    parser = argparse.ArgumentParser(
+        description=""" 
+        GitLab Project Export is
+        small project using Gitlab API for exporting whole gitlab
+        project with wikis, issues etc.
+        Good for migration or simple backup your gitlab projects.
+        """,
+        epilog='Created by Robert Vojcik <robert@vojcik.net>')
+    
+    # Arguments
+    parser.add_argument('-c', dest='config', default='config.yaml',
+                       help='config file') 
+    parser.add_argument('-f', dest='filepath', default=False,
+                       help='Path to gitlab exported project file') 
+    parser.add_argument('-p', dest='project_path', default=False,
+                       help='Project path') 
+    parser.add_argument('-d', dest='debug', default=False, action='store_const', const=True,
+                       help='Debug mode') 
+    
+    args = parser.parse_args()
+
+    if not os.path.isfile(args.config):
+        print("Unable to find config file %s" % (args.config)) 
+
+    c = config.Config(args.config)
+    token = c.config["gitlab"]["access"]["token"]
+    gitlab_url = c.config["gitlab"]["access"]["gitlab_url"]
+
+    # Init gitlab api object
+    if args.debug:
+        print("%s, token"%(gitlab_url))
+    gitlab = gitlab.Api(gitlab_url, token)
+
+    # import project
+    if args.project_path and args.filepath and os.path.isfile(args.filepath):
+        if args.debug:
+            print("Exporting %s"%(args.project_path))
+        status = gitlab.project_import(args.project_path, args.filepath)
+
+        # Import successful
+        if status:
+            print("Import success for %s"%(args.project_path))
+            sys.exit(0)
+        else:
+            print("Import was not successful")
+            sys.exit(1)
+
+    else:
+        print("Error, you have to specify correct project_path and filepath")
+        sys.exit(1)
+
--- lib/gitlab.py
+++ lib/gitlab.py
@@ -3,6 +3,7 @@ import requests
 import urllib
 import sys
 import time
+import os
 
 class Api:
     '''Api class for gitlab'''
@@ -18,11 +19,24 @@ class Api:
         self.download_url = None
         return requests.post(self.api_url+"/projects/" + project_url + "/export", headers=self.headers)
 
+    def __api_import(self, project_name, namespace, filename):
+        '''Send import request to API'''
+        data = { "path": project_name, "namespace": namespace, "overwrite": True}
+        return requests.post(self.api_url+"/projects/import", data=data, files={"file": open(filename, 'r')}, headers=self.headers)
+
     def __api_status(self,project_url):
         '''Check project status'''
         return requests.get(self.api_url+"/projects/" + project_url + "/export", headers=self.headers)
 
+    def __api_import_status(self,project_url):
+        '''Check project import status'''
+        return requests.get(self.api_url+"/projects/" + project_url + "/import", headers=self.headers)
+
     def project_export(self, project_path):
+        ''' Export Gitlab project
+        When project export is finished, store download URLs 
+        in objects variable download_url ready to be downloaded'''
+
         url_project_path = urllib.quote(project_path, safe='')
         
         # Let's export project
@@ -64,9 +78,55 @@ class Api:
             else:
                 return False
                     
-
-
         else:
             print("API not respond well with %s" %(str(r.status_code)), file=sys.stderr) 
+            print(r.text, file=sys.stderr)
             return False
 
+    def project_import(self, project_path, filepath):
+        ''' Import project to GitLab from file'''
+        url_project_path = urllib.quote(project_path, safe='')
+        project_name = os.path.basename(project_path)
+        namespace = os.path.dirname(project_path)
+        
+        # Let's import project
+        r = self.__api_import(project_name, namespace, filepath)
+        if ( str(r.status_code) == "202" ):
+            # Api good, check for status
+            s = ""
+            status_export = False
+            while True:
+                r = self.__api_import_status(url_project_path)
+
+                # Check API reply status
+                if ( r.status_code == requests.codes.ok ):
+                    json = r.json()
+
+                    # Check export status
+                    if "import_status" in json.keys():
+                        s = json["import_status"]
+                        if s == "finished":
+                            status_import = True
+                            break
+                        elif s == "failed":
+                            status_import = False
+                            break
+                    else:
+                        s = "unknown"
+                    
+                else:
+                    print("API not respond well with %s" %(str(r.status_code)), file=sys.stderr)
+                    break
+
+                # Wait litle bit
+                time.sleep(2)
+
+            if status_import:
+                return True
+            else:
+                return False
+                    
+        else:
+            print("API not respond well with %s" %(str(r.status_code)), file=sys.stderr) 
+            print(r.text, file=sys.stderr)
+            return False
